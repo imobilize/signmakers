425a5a9ca69582f44b5d4d508442714c21b80b75
Jarrar Shah <80749329+JarrarShah@users.noreply.github.com>
2024-07-17 19:11:49 +0500

    fixes (1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
-// tailwind.config.js
 module.exports = {
-  content: ['./layouts/**/*.html', './content/**/*.md'],
+  content: [
+    './layouts/**/*.html',
+    './content/**/*.md',
+    './assets/js/**/*.js',
+  ],
   darkMode: 'class',
   theme: {
     extend: {
@@ -27,8 +30,8 @@ module.exports = {
         'sm': '0.875rem',    // Small
         'base': '1rem',      // Base
         'lg': '1.125rem',    // Large
-        'xl': '1rem',     // Extra Large
-        '1xl': '1rem',     // 1 Extra Large
+        'xl': '1rem',        // Extra Large
+        '1xl': '1rem',       // 1 Extra Large
         '2xl': '1.5rem',     // 2 Extra Large
         'special' : '1.50rem',
         '3xl': '1.875rem',   // 3 Extra Large
