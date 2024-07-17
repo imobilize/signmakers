2a9c885aa319051fa5cdb05c0ae8e3c98d71dd66
Jarrar Shah <80749329+JarrarShah@users.noreply.github.com>
2024-07-17 16:32:36 +0500

    minor fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
         'lg': '1.125rem',    // Large
         'xl': '1rem',     // Extra Large
         '1xl': '1rem',     // 1 Extra Large
-        '2xl': '1rem',     // 2 Extra Large
+        '2xl': '1.5rem',     // 2 Extra Large
         'special' : '1.50rem',
         '3xl': '1.875rem',   // 3 Extra Large
         '4xl': '2.25rem',    // 4 Extra Large
